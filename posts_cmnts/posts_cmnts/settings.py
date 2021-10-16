9d9dff34640aef2202360f1d45ddf4489fefc6fc
anna <anuta.gomes@gmail.com>
2021-10-16 06:58:12 +0300

    docker-compose is done

--- posts_cmnts/posts_cmnts/settings.py
+++ posts_cmnts/posts_cmnts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)!xb_-8-pfw7vqc7f-p6z*vha0w_*)ympmrd(h7_x)%_hjrm1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'posts_cmnts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'posts_cmnts',
+        'USER': 'anna',
+        'PASSWORD': 'Asdfjkl;47813',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
